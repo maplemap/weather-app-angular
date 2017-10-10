a9aadfb3f4d10208d3067ec1c6aaf17060a1db56
Sergey.Illarionov <easthunch@gmail.com>
2017-10-10 12:39:02 +0300

    update: to city

---
+++ src/app/interfaces/weather.ts
@@ -0,0 +1,10 @@
+export class Weather {
+  constructor(
+    public city: string,
+    public temp: string,
+    public type: string,
+    public tempMin: string,
+    public tempMax: string,
+    public img: string
+  ) {}
+}
