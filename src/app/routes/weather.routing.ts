0d5d930bb88ab932366ccc0fb7cfbb5d36b18e5e
Sergey.Illarionov <easthunch@gmail.com>
2017-10-10 12:37:34 +0300

    add: routing

--- src/app/routes/weather.routing.ts
+++ src/app/routes/weather.routing.ts
@@ -1,10 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 
+import { ResolveLocationService } from '../services/resolve-location.service';
+
 import { WeatherComponent } from '../weather/weather.component';
 
 const WEATHER_ROUTER: Routes = [
-  { path: '', component: WeatherComponent }
+  { path: '', component: WeatherComponent, resolve: { weather: ResolveLocationService }}
 ]
 
 export const weatherRouting: ModuleWithProviders = RouterModule.forRoot(WEATHER_ROUTER)
