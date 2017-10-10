9040ce401a2851c67ffe11260f44e0deeb36f13b
Sergey.Illarionov <easthunch@gmail.com>
2017-10-10 12:39:27 +0300

    pass: 'weather' property

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { WeatherService } from '../services/weather.service';
+import { Weather } from '../interfaces/weather';
 
 @Component({
   selector: 'app-weather',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather.component.scss']
 })
 export class WeatherComponent implements OnInit {
+  weather: Weather;
+  // weatherClass: Weather;
+  location: any = {};
 
-  constructor() { }
+  constructor(private weatherService: WeatherService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.data.subscribe(
+      (data: { weather: Weather }) => {
+        this.weather = data.weather;
+      }
+    )
   }
 
 }
