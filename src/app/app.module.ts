33f2aef199000c3b56ae030a13eac4233c5cbb38
Sergey.Illarionov <easthunch@gmail.com>
2017-10-11 16:32:48 +0300

    add: search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -27,6 +28,7 @@ import { LoaderService } from './components/loader/loader.service';
   imports: [
     BrowserModule,
     HttpModule,
+    FormsModule,
     weatherRouting
   ],
   providers: [
--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.scss']
 })
-export class SearchBarComponent implements OnInit {
+export class SearchBarComponent {
+  searchText: string = '';
 
-  constructor() { }
-
-  ngOnInit() {
+  onClickCleanBtn() {
+    this.searchText = '';
   }
-
 }
