4b871d76c9b9f0724bebf5433b1db914a9292a46
Sergey.Illarionov <easthunch@gmail.com>
2017-10-17 21:31:24 +0300

    add: changeUnit method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  changeUnit(unitStatus: boolean) {
+    console.log(unitStatus);
+  }
 }
