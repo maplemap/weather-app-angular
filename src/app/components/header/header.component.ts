97cbdc9dbe4c41c60374cb964ad2623dcd3c19c6
Sergey.Illarionov <easthunch@gmail.com>
2017-10-17 21:31:12 +0300

    add: input element

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  @Output() changeUnit: EventEmitter<boolean> = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit() {
+  onChangeUnitSwitcher(e: any) {
+    this.changeUnit.emit(e.target.checked);
   }
-
 }
