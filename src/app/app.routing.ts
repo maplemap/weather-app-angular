29cf437fba112c96f6cecbb9199c92ea9f5fdeab
Sergey.Illarionov <easthunch@gmail.com>
2017-10-23 17:41:28 +0300

    add: search and default routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,10 +4,14 @@ import { ModuleWithProviders } from '@angular/core';
 import { ResolveLocationService } from './_services/resolve-location.service';
 import { ResolveCityService } from './_services/resolve-city.service';
 import { WeatherComponent } from './weather/weather.component';
+import { ErrorComponent } from './components/error/error.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const APP_ROUTER: Routes = [
   { path: '', component: WeatherComponent, resolve: { weather: ResolveLocationService }},
-  { path: ':city', component: WeatherComponent, resolve: { weather: ResolveCityService }}
+  { path: ':city', component: WeatherComponent, resolve: { weather: ResolveCityService }},
+  { path: 'service/search', component: NotFoundComponent },
+  { path: '**', component: ErrorComponent, data: { title: '404 Not Found', message: 'You may be lost. Follow the breadcrumbs back <a href="/">home</a>.' } }
 ]
 
 export const appRouting: ModuleWithProviders = RouterModule.forRoot(APP_ROUTER)
