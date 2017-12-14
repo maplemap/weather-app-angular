902d9fded973e7e3eb7a7fc2f7f278d516a956ca
Serhiy Illarionov <sergio.illarionov@gmail.com>
2017-12-14 13:15:00 +0300

    refactoring: clock service

--- src/app/clock/clock.service.ts
+++ src/app/clock/clock.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ClockService {
