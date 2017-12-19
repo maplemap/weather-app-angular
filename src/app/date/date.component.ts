c5af509bebc93ae47c36d27bfcecad66475e9715
Serhiy Illarionov <sergio.illarionov@gmail.com>
2017-12-19 13:29:00 +0300

    fix: ngOnDestroy

--- src/app/date/date.component.ts
+++ src/app/date/date.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DateService } from './date.service';
 import { Subscription } from 'rxjs/Subscription';
 
@@ -7,7 +7,7 @@ import { Subscription } from 'rxjs/Subscription';
   templateUrl: './date.component.html',
   styleUrls: ['./date.component.scss']
 })
-export class DateComponent implements OnInit {
+export class DateComponent implements OnInit, OnDestroy {
   private _clockSubscription: Subscription;
   date: string;
 
@@ -17,5 +17,9 @@ export class DateComponent implements OnInit {
     this._clockSubscription = this.dateService.getDate().subscribe(
       date => this.date = this.dateService.formatDate(date)
     );
+  }
+
+  ngOnDestroy(): void {
+    this._clockSubscription.unsubscribe();
   }
 }
